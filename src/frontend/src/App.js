b27eb12fa2462533ed2de87d799b78a2d9a0e661
duhan <duhan@189.cn>
2025-09-30 09:13:25 +0800

    .

---
+++ src/frontend/src/App.js
@@ -0,0 +1,49 @@
+// App.js
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      {/* 主显示区域 */}
+      <div className="main-display">
+        <iframe
+          title="VNC Viewer"
+          src="#"
+          className="vnc-iframe"
+        >
+          您的浏览器不支持 iframe。
+        </iframe>
+      </div>
+
+      {/* 右侧操作区域 */}
+      <div className="right-panel">
+        <div className="button-group">
+          <button>信息1</button>
+          <button>信息2</button>
+          <button>信息3</button>
+          <button>信息4</button>
+        </div>
+        <div className="execution-info">
+          执行信息显示区
+        </div>
+      </div>
+
+      {/* 底部操作区域 */}
+      <div className="bottom-panel">
+        <div className="tabs">
+          <button className="tab-button">代码</button>
+          <button className="tab-button">命令</button>
+        </div>
+        <input
+          type="text"
+          placeholder="请输入内容"
+          className="input-box"
+        />
+        <button className="send-button">发送</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
