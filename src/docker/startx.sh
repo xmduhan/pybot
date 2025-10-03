d7c2163cde206d616c7bc34dfe90716e70802016
duhan <duhan@189.cn>
2025-10-03 18:42:29 +0800

    .

--- src/docker/startx.sh
+++ src/docker/startx.sh
@@ -2,8 +2,10 @@ sleep 5
 touch /root/.Xauthority
 xauth generate :0 . trusted
 startxfce4&
-while true; do
-  xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true
-  sleep 10
-  echo 'xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true'
-done
+sleep 60
+xset s off
+xset s noblank
+xset s noexpose
+xset -dpms
+xset q
+sleep infinity
