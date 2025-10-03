48e87d0e8ed2cf7728a44a591d6d50023ca03c2c
duhan <duhan@189.cn>
2025-10-03 17:32:32 +0800

    .

--- src/docker/startx.sh
+++ src/docker/startx.sh
@@ -1,4 +1,6 @@
 sleep 5
 touch /root/.Xauthority
 xauth generate :0 . trusted
-startxfce4
+startxfce4&
+sleep 5
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s true
